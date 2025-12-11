4d008bf70345b70808db9af4cbcc920b9a364db0
kim dong hun <nkdonghun712@gmail.com>
2025-12-11 17:35:42 +0900

    add github actions for ci/cd automation

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // 👇 여기 본인 레포지토리 이름이 정확한지 꼭 확인하세요!
-  // 앞뒤로 슬래시(/)가 반드시 있어야 합니다.
   base: "/pb_Netflixdemo_kdh/", 
-  build: {
-    outDir: "docs",
-  },
 })
